e9049659adab0f9ef1f30a92be83fd07ee82610e
Juan Ignacio Elizeche Armoa <94756745+ignacioelizeche@users.noreply.github.com>
2025-11-10 09:47:43 -0800

    Modify sftp_copy to return zip file on download
    Refactor sftp_copy to return a zip file response

--- sftptools_app.py
+++ sftptools_app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from services.sftp_service import download_from_sftp
 
@@ -6,9 +6,7 @@ app = FastAPI(
     title="SFTP Tools API",
     docs_url="/docs",
     openapi_url="/openapi.json",
-    servers=[
-        {"url": "/sftptools", "description": "SFTP Tools Server"}
-    ]
+    servers=[{"url": "/sftptools", "description": "SFTP Tools Server"}]
 )
 
 class SFTPRequest(BaseModel):
@@ -20,14 +18,16 @@ class SFTPRequest(BaseModel):
 @app.post("/sftpcopy")
 async def sftp_copy(request: SFTPRequest):
     try:
-        download_from_sftp(
+        zip_buffer = download_from_sftp(
             host=request.host,
             username=request.username,
             password=request.password,
             directory=request.directory,
             download_path="./downloads"
         )
-        return {"message": "Files downloaded successfully"}
+
+        headers = {"Content-Disposition": "attachment; filename=archivos_descargados.zip"}
+        return Response(content=zip_buffer.read(), media_type="application/zip", headers=headers)
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
