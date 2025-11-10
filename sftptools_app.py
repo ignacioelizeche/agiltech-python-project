893a8d13bd6c7b4994d4b7d1cd19b0d4b9074548
Juan Ignacio Elizeche Armoa <94756745+ignacioelizeche@users.noreply.github.com>
2025-11-10 11:45:33 -0800

    Update SFTPRequest model and download logic

--- sftptools_app.py
+++ sftptools_app.py
@@ -3,32 +3,37 @@ from pydantic import BaseModel
 from services.sftp_service import download_from_sftp
 from fastapi.responses import Response
 from typing import List, Optional
+import os
 
 app = FastAPI(title="SFTP Tools API")
+
+BASE_DOWNLOAD_PATH = "/opt/agiltech-python-project/downloads"
 
 class SFTPRequest(BaseModel):
     host: str
     directory: str
-    descarga_path: str
+    destination_folder: str           # nombre de carpeta que creará bajo downloads
     username: str
     password: str
     filename_startswith: Optional[List[str]] = []  # array de prefijos
-    from_date: Optional[str] = ""  # YYYY-MM-DD
+    from_date: Optional[str] = ""                  # fecha mínima YYYY-MM-DD
 
 @app.post("/sftpcopy")
 async def sftp_copy(request: SFTPRequest):
     try:
+        download_path = os.path.join(BASE_DOWNLOAD_PATH, request.destination_folder)
+
         zip_buffer = download_from_sftp(
             host=request.host,
             username=request.username,
             password=request.password,
             directory=request.directory,
-            download_path=request.descarga_path,
+            download_path=download_path,
             filename_startswith=request.filename_startswith,
             from_date=request.from_date
         )
 
-        headers = {"Content-Disposition": "attachment; filename=archivos_descargados.zip"}
+        headers = {"Content-Disposition": f"attachment; filename={request.destination_folder}_archivos.zip"}
         return Response(content=zip_buffer.read(), media_type="application/zip", headers=headers)
 
     except Exception as e:
